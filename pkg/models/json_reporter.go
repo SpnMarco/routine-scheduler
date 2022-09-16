733ccddef40ab5b89f0e24c4db909b53c302ed57
Marco Spina <spina.marco@outlook.it>
2022-09-16 17:29:39 +0200

    clean json file model

--- pkg/models/json_reporter.go
+++ pkg/models/json_reporter.go
@@ -1,17 +1,5 @@
 package models
 
-// {
-// 	"database_clean_routine": {
-// 	  "run_no": 13,
-// 	  "run_errors": 0
-// 	},
-// 	"logs_clean_routine": {
-// 	  "run_no": 4,
-// 	  "run_errors": 1
-// 	}
-// 	...
-//   }
-
 type databaseCleanRoutine struct {
 	RunNo     int `json:"run_no"`
 	RunErrors int `json:"run_errors"`
